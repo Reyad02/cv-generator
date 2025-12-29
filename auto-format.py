c137797fc71287980f5e18873312f846067cb557
Al-Momen Reyad <almomenreyad@gmail.com>
2025-12-29 16:44:08 +0600

    auto-format file updated. Taking input in proper format

--- auto-format.py
+++ auto-format.py
@@ -11,23 +11,48 @@ client = OpenAI(api_key=env_vars.get("OPENAI_API_KEY"))
 app = FastAPI(title="Resume Builder AI")
 
 system_prompt = """
-Yor are an CV expert AI assistant. You should format the user details for CV. You should check any spelling mistakes, grammar mistakes etc. You also refine the about me and other description sections to make them more professional and impactful. For format the details use only '\n' for new lines not any bullets or dashes.
+You are a CV expert AI assistant. You should format the user details for CV. You should check any spelling mistakes, grammar mistakes etc. You also refine the about me and other description sections to make them more professional and impactful. For format the details use only '\n' for new lines not any bullets or dashes.
 User details 
 ------------------
 {details}
 
-Return ONLY this JSON structure:
+Required JSON format:
 {{
-  "refine_content": "refined user details with professional tone",
+  "refine_content": "refined user details with professional tone"
 }}
 """
 
-class UserDetailsRequest(BaseModel):
-    details: str
+class WorkExperience(BaseModel):
+    job_title: str
+    role: str
+    company_name: str
+    start_date: str
+    end_date: str
+    responsibility: str
     
+class Education(BaseModel):
+    degree: str
+    institution: str
+    start_year: str
+    end_year: str
+    results: str | None
+
+
+class UserDetailsRequest(BaseModel):
+    name: str
+    email: str
+    phone: str
+    work_exp: list[WorkExperience]
+    location: str
+    photo: str
+    about_me: str
+    skills: list[str]
+    educations: list[Education]
+ 
 @app.post("/refine_resume")
 async def refine_resume(request: UserDetailsRequest):
-    prompt = system_prompt.format(details=request.details)
+    user_details_text = json.dumps(request.model_dump(), indent=2)
+    prompt = system_prompt.format(details=user_details_text)
     
     response = client.responses.create(
         model="gpt-5-nano",
@@ -36,9 +61,6 @@ async def refine_resume(request: UserDetailsRequest):
             {"role": "user", "content": prompt}
         ],
         reasoning={"effort": "low"},
-
-        # max_tokens=1000,
-        # temperature=0.7,
     )
     
     try:
